f52aeb472dc442d746077e9214467f5f691409c3
Vinay Gummadavelli <vinaygummadavelli@Vinay-G.local>
2015-10-19 10:45:53 -0700

    IG-1 moved Footer into mw-footer

--- scripts/sections/footer.ts
+++ scripts/sections/footer.ts
@@ -1,5 +1,8 @@
 $("./body") {
   insert("footer", "Powered by Moovweb", class: "mw-footer") {
+  	# Ticket IG-1 - Moved the Footer into mw-footer
+  	move_here("preceding-sibling::div[@id='Footer']", 'top')
+
     # Move stuff here
   }
 }
